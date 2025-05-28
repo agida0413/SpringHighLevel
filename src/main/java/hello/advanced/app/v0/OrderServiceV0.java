99b165fbacee75f5e21601306d40e6d7152f39b0
kimyj <yongjjun8718@gmail.com>
2025-05-28 21:42:26 +0900

    mvc

--- src/main/java/hello/advanced/app/v0/OrderServiceV0.java
+++ src/main/java/hello/advanced/app/v0/OrderServiceV0.java
@@ -1,20 +1,15 @@
-package hello.advanced.v0;
+package hello.advanced.app.v0;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class OrderService {
+public class OrderServiceV0 {
 
-    private final OrderRepository orderRepository;
+    private final OrderRepositoryV0 orderRepository;
 
-    public void orderItem(String itemId){
+    public void orderItem(String itemId) {
         orderRepository.save(itemId);
-
-
-
-
-
     }
 }
